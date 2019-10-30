321ca416fe4e7397963e3388b61a9de62800bd5c
louis <wangpd1990@163.com>
2019-10-30 10:26:03 +0100

    Parameters of the SN curve and Haigh diagram saved to .txt file.

--- GUI/SN_Cal_GUI.py
+++ GUI/SN_Cal_GUI.py
@@ -12,10 +12,10 @@
 import sys
 import time
 import matplotlib.pyplot as plt
-from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5 import QtGui, QtWidgets
 from PyQt5.QtWidgets import QMessageBox
 from math import log10, sqrt
-from GUI import SNCurve
+import SNCurve
 
 MainWindow_main = SNCurve.Ui_SN_Curve
 
@@ -35,63 +35,63 @@ class Main(QtWidgets.QMainWindow, MainWindow_main):
         """
         # Para Read
         try:
-            self.mat = str(self.l_mat.currentText())
-            self.Rp = float(self.l_Rp.text())
-            self.Rm = float(self.l_Rm.text())
-            self.t = float(self.l_t.text())
-            self.Rz = float(self.l_Rz.text())
-            self.j = float(self.l_j.text())
-            self.j_0 = float(self.l_j_0.text())
-            self.S_pu = float(self.l_S_pu.text())
-            self.gamma_M = float(self.l_gamma_M.text())
-            self.sigm_b = self.Rm * 1.06
-            self.R = -1
+            mat = str(self.l_mat.currentText())
+            rp = float(self.l_Rp.text())
+            rm = float(self.l_Rm.text())
+            t = float(self.l_t.text())
+            rz = float(self.l_Rz.text())
+            j = float(self.l_j.text())
+            j_0 = float(self.l_j_0.text())
+            s_pu = float(self.l_S_pu.text())
+            gamma_m = float(self.l_gamma_M.text())
+            sigma_b = rm * 1.06
+            R = -1
             if self.check_t_c.isChecked():
-                self.sign_t = 1
+                sigma_t = 1
             else:
-                self.sign_t = 0
+                sigma_t = 0
         except Exception as e:
             with open('ErrorLog.txt', 'a') as f:
-                f.write(time.strftime("%Y-%m-%d-%H-%M", time.localtime(time.time())) + '\n')
+                f.write(time.strftime("\n%Y-%m-%d-%H-%M", time.localtime(time.time())) + '\n')
                 f.write(repr(e))
                 f.close()
-                QMessageBox.warning(self, 'Error', '参数输入错误，请检查！\n')
-            return
+                QMessageBox.warning(self, 'Error', '参数输入错误，请检查！')
+            # return
 
         # Calculation
-        if self.mat == 'GGG':
-            self.M = 0.00035 * self.sigm_b + 0.08
+        if mat == 'GGG':
+            M = 0.00035 * sigma_b + 0.08
         else:
-            self.M = 0.00035 * self.sigm_b + 0.05
+            M = 0.00035 * sigma_b + 0.05
 
         # surface roughness factor
-        F_o = 1 - 0.22 * (log10(self.Rz) ** 0.64) * log10(self.sigm_b) + 0.45 * (log10(self.Rz) ** 0.53)
+        F_o = 1 - 0.22 * (log10(rz) ** 0.64) * log10(sigma_b) + 0.45 * (log10(rz) ** 0.53)
 
         # notch factor
-        alph_k = 1
+        alpha_k = 1
         n = 1
-        belt_k = alph_k / n
+        belt_k = alpha_k / n
 
         # total influence factor fok
         F_ok = sqrt(belt_k ** 2 - 1 + 1 / (F_o ** 2))
 
         # fatigue strength of specimen
-        if self.mat == 1:
-            sigm_w = 0.27 * self.sigm_b + 100
+        if mat == 1:
+            sigma_w = 0.27 * sigma_b + 100
         else:
-            sigm_w = 0.27 * self.sigm_b + 85
+            sigma_w = 0.27 * sigma_b + 85
 
         # fatigue strength of component
-        sigm_wk = sigm_w / F_ok
+        sigm_wk = sigma_w / F_ok
 
         # slopes of SN curve m1 and m2
-        self.m1 = 5.5 / (F_ok ** 2) + 6
-        self.m2 = 2 * self.m1 - 1
+        m1 = 5.5 / (F_ok ** 2) + 6
+        m2 = 2 * m1 - 1
 
         # factor for influence of mean stress
-        u = 1 / (self.M + 1) * sigm_wk / self.sigm_b
-        a = (1 + self.R) / (1 - self.R) * sigm_wk / self.sigm_b
-        p = (1 / (self.M + 1) - 1 + u ** 2) / (u ** 2 - u)
+        u = 1 / (M + 1) * sigm_wk / sigma_b
+        a = (1 + R) / (1 - R) * sigm_wk / sigma_b
+        p = (1 / (M + 1) - 1 + u ** 2) / (u ** 2 - u)
         if a == 0:
             Fm = 1
         else:
@@ -103,44 +103,78 @@ class Main(QtWidgets.QMainWindow, MainWindow_main):
                     1 / (1 - p) / a ** 2 + ((1 + p * a) / 2 / a ** 2 / (1 - p)) ** 2)
 
         # stress amplitude at knee of SN curve
-        sigm_A = sigm_wk * Fm
+        sigma_A = sigm_wk * Fm
 
         # number of load cycles at knee of SN curve
-        self.N_D = 10 ** (6.8 - 3.6 * (1 / self.m1))
+        N_D = 10 ** (6.8 - 3.6 * (1 / m1))
 
         # upgrading factors
-        # quelity level
-        S_d = 0.85 ** (self.j - self.j_0)
-        # thickness-dependent tensile value Rm
-        S_t = (self.t / 25) ** ((-0.15) * self.sign_t)
+        # quality level
+        S_d = 0.85 ** (j - j_0)
+        # thickness-dependent tensile value rm
+        S_t = (t / 25) ** ((-0.15) * sigma_t)
         # total upgrading factor
-        S = self.S_pu * S_d * S_t
+        S = s_pu * S_d * S_t
 
         # upgraded stress amplitude at knee of SN curve
-        self.sigm_d = sigm_A * S / self.gamma_M
+        sigma_d = sigma_A * S / gamma_m
+        sigma_p = sigma_d/(M + 1)
         # upper limit of fatigue life line
-        self.sigm_1 = self.Rp * (1 - self.R) / self.gamma_M
+        sigma_1 = rp * (1 - R) / gamma_m
         # number of load cycles at upper fatigue limit
-        self.N_1 = self.N_D * (2 * self.sigm_d / self.sigm_1) ** self.m1
-        # self.N_2 = 5 * 10 ** 6
-        # self.sigm_2 = (self.N_D / self.N_2) ** (1 / self.m2) * self.sigm_d
-        self.N_e = 10 ** 8
-        self.sigm_e = (self.N_D / self.N_e) ** (1 / self.m2) * self.sigm_d
+        N_1 = N_D * (2 * sigma_d / sigma_1) ** m1
+        N_e = 10 ** 8
+        sigma_e = (N_D / N_e) ** (1 / m2) * sigma_d
 
-        plt.figure()
-        x = [0, self.N_1, self.N_D, self.N_e]
-        y = [self.sigm_1, self.sigm_1, self.sigm_d, self.sigm_e]
+        # SN curve parameter written to txt file.
+        with open('SN_Curve.txt', 'w') as f:
+            f.write('Summary of the SN curve Parameter\n')
+            f.write('Material:'+mat+'\n')
+            f.write('Stress ratio R:'+str(R)+'\n')
+            f.write('*'*30+'\n')
+            f.write('Calculation of the SN curve according to the GL2010 5.B.3.1 and 5.B.3.2.\n')
+            f.write('Tensile strength:\n')
+            f.write(str(rm)+'\n')
+            f.write('Yield Strength:\n')
+            f.write(str(rp)+'\n')
+            f.write('Alternating stress limit (Amplitude/Range):\n')
+            f.write(str(round(sigma_d, 3))+'/'+str(round(2*sigma_d, 3))+'\n')
+            f.write('Pulsating stress limit (Amplitude/Range):\n')
+            f.write(str(round(sigma_p, 3))+'/'+str(round(2*sigma_p, 3))+'\n')
+            f.write('Slope of the SN curve (Left/Right of the knee point):\n')
+            f.write(str(round(m1, 3))+'/'+str(round(m2, 3))+'\n')
+            f.write('Number of load cycles at knee of SN curve:\n')
+            f.write(str('%.3e' % N_D)+'\n')
+            f.write('Mean stress sensitivity:\n')
+            f.write(str(round(M, 3))+'\n')
+            f.write('*'*30+'\n')
+            f.write('Parameter of the Haigh Diagram:\n')
+            f.write('Mean stress / Amplitude:\n')
+            f.write(str(round(rp/gamma_m, 3))+'/'+'0\n')
+            if mat == 'GGG':
+                f.write(str(round((rp / gamma_m - sigma_d) / (1 - M), 3))+'/'+str(round(M*(rp / gamma_m - sigma_d) /
+                                                                                        (M - 1) + sigma_d, 3))+'\n')
+            else:
+                f.write('')
+            f.write(str(round(sigma_p, 3))+'/'+str(round(sigma_p, 3))+'\n')
+            f.write('0'+'/'+str(round(sigma_d, 3))+'\n')
+            f.write(str(round(sigma_d / (M - 1), 3)) + '/' + str(round(-sigma_d / (M - 1), 3))+'\n')
+            f.write(str(round(-rp/gamma_m, 3))+'/'+'0\n')
+            f.close()
+        # Plot of the SN curve
+        x = [0, N_1, N_D, N_e]
+        y = [sigma_1, sigma_1, sigma_d, sigma_e]
         plt.loglog(x, y, lw=2, marker='*')
         plt.xlabel('Cycle Numbers')
         plt.ylabel('Stress Amplitude/MPa')
         plt.xlim(10, 10 ** 8)
         plt.yticks([10, 100, 1000])
-        plt.annotate(s='(%.1e,%.2f)' % (self.N_1, self.sigm_1), xy=(self.N_1, self.sigm_1))
-        plt.annotate(s='(%.1e,%.2f)' % (self.N_D, self.sigm_d), xy=(self.N_D, self.sigm_d))
-        plt.annotate(s='m1=%.2f' % self.m1, xy=(10 ** 3, 142))
-        plt.annotate(s='m2=%.2f' % self.m2, xy=(10 ** 6.7, 40))
+        plt.annotate(s='(%.1e,%.2f)' % (N_1, sigma_1), xy=(N_1, sigma_1))
+        plt.annotate(s='(%.1e,%.2f)' % (N_D, sigma_d), xy=(N_D, sigma_d))
+        plt.annotate(s='m1=%.2f' % m1, xy=(10 ** 3, 142))
+        plt.annotate(s='m2=%.2f' % m2, xy=(10 ** 6.7, 40))
         plt.show()
-        return
+        # return
 
 
 if __name__ == "__main__":
